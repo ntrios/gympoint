29b8b012794ba374b0bee3b5e96330358184c801
Nathan Rios <nathan.cdc@gmail.com>
2019-10-25 18:23:42 -0300

    create sessions

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,9 +2,21 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const user = await User.create(req.body);
+    // check if exist email in BD
+    const userExists = await User.findOne({ where: { email: req.body.email } });
 
-    return res.json(user);
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name, email, admin } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      admin,
+    });
   }
 }
 
