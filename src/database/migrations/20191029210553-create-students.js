0fee0477cfc47bb0ac3eb7c54bf71bb5b8f14368
Nathan Rios <nathan.cdc@gmail.com>
2019-10-29 18:24:28 -0300

    terminar em casa

---
+++ src/database/migrations/20191029210553-create-students.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      idade: {
+        type: Sequelize.INTENGER,
+        allowNull: false,
+      },
+      peso: {
+        type:
+      }
+
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  },
+};
