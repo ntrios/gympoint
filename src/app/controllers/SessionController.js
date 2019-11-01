4106d565847e7c83adb86a1520efa02045cd4a90
Nathan Rios <nathan.cdc@gmail.com>
2019-11-01 07:14:39 -0300

    fix user mode controller

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,14 +2,11 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/User';
-import Student from '../models/Student';
 
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    let subject;
-
     const schema = Yup.object().shape({
       admin: Yup.boolean(),
       email: Yup.string()
@@ -24,50 +21,26 @@ class SessionController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const { admin, email, password } = req.body;
+    const { email, password } = req.body;
 
-    if (admin) {
-      const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { email } });
 
-      if (!user) {
-        return res.status(401).json({ error: 'User not found.' });
-      }
-
-      if (!(await user.checkPassword(password))) {
-        return res.status(401).json({ error: 'Password does not match' });
-      }
-    } else if (!admin) {
-      const user = await Student.findOne({ where: { email } });
-
-      if (!user) {
-        return res.status(401).json({ error: 'Student not found.' });
-      }
-
-      const { id } = user;
-
-      // console.log(user.id);
-      // console.log(user.name);
-      // console.log(user.email);
-
-      // this.subject = user;
-      // console.log(user);
-      // console.log(this.subject);
+    if (!user) {
+      return res.status(401).json({ error: 'User not found.' });
     }
-    console.log(this.id);
 
-    // console.log(subject);
-    // const { id, name } = subject;
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
 
-    // // get access data user
-    // // const { email, password } = req.body;
+    const { id, name } = user;
 
-    // return res.json({
-    //   user: { id, name, email },
-    //   admin: { admin },
-    //   token: jwt.sign({ id }, authConfig.secret, {
-    //     expiresIn: authConfig.expiresIn,
-    //   }),
-    // });
+    return res.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
