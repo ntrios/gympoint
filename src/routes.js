f774bb027d2d2bb3144f10b6a18bddc183bea72d
Nathan Rios <nathan.cdc@gmail.com>
2019-11-18 11:47:37 -0300

    feature plans

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,4 +17,8 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/plans', PlanController.index);
+routes.post('/plans', PlanController.store);
+routes.put('/plans', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
 module.exports = routes;
